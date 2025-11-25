70602e30f152666785ff70cc919aaecb0e7c51a3
beregaal <bere_gaal@hotmail.com>
2025-11-25 14:50:38 -0600

    Implementación SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+
 
 // https://astro.build/config
 //export default defineConfig({});
 export default defineConfig({
-  site: 'https://demo.gaallabs.com',
+  site: "https://demo.gaallabs.com",
+  integrations: [
+    sitemap({
+      entryLimit: 5000, // URLs por archivo
+      filter: () => true,
+      serialize(item) {
+        return {
+          ...item,
+          lastmod: new Date().toISOString(),
+        };
+      },
+    }),
+  ],
 });
